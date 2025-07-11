b396d5f467cdede0bfe805c76312ec321d7d3910
Romain77250 <niclot.romain@gmail.com>
2025-07-11 18:17:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CampaignPage from "./CampaignPage";
+import PageCampagne from "./PageCampagne";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CampaignPage />
+    <PageCampagne />
   </React.StrictMode>
 );
